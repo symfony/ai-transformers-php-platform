494b4046b1b2736d7aaf17a59e2e875dbd94cb5d
Muhammad Elhwawshy <m.elhwawshy@gmail.com>
2026-01-05 22:32:20 +0100

    [AI Bundle][Platform][TransformersPhp] Convert vector result

--- ResultConverter.php
+++ ResultConverter.php
@@ -15,8 +15,11 @@ use Codewithkyrian\Transformers\Pipelines\Task;
 use Symfony\AI\Platform\Model;
 use Symfony\AI\Platform\Result\ObjectResult;
 use Symfony\AI\Platform\Result\RawResultInterface;
+use Symfony\AI\Platform\Result\ResultInterface;
 use Symfony\AI\Platform\Result\TextResult;
+use Symfony\AI\Platform\Result\VectorResult;
 use Symfony\AI\Platform\ResultConverterInterface;
+use Symfony\AI\Platform\Vector\Vector;
 
 final class ResultConverter implements ResultConverterInterface
 {
@@ -25,14 +28,21 @@ final class ResultConverter implements ResultConverterInterface
         return true;
     }
 
-    public function convert(RawResultInterface $result, array $options = []): TextResult|ObjectResult
+    public function convert(RawResultInterface $result, array $options = []): ResultInterface
     {
         $data = $result->getData();
+        $task = $options['task'] ?? null;
 
-        if (Task::Text2TextGeneration === $options['task']) {
+        if (Task::Text2TextGeneration === $task) {
             $result = reset($data);
 
             return new TextResult($result['generated_text']);
+        }
+
+        if (Task::Embeddings == $task || Task::FeatureExtraction == $task) {
+            return new VectorResult(
+                ...array_map(static fn (array $vector) => new Vector($vector), $data),
+            );
         }
 
         return new ObjectResult($data);
